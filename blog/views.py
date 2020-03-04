5bf0c614e1587916eb3dd3be05c0297d24f953e2
Pablo61098 <pablosolano61098@gmail.com>
2020-03-04 18:13:24 -0500

    Heroku Config

--- blog/views.py
+++ blog/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 
 from django.views.generic import ListView, DetailView
 from .models import Post
-
-
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
 class BlogListView(ListView):
     model = Post
@@ -13,6 +13,20 @@ class BlogDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
 
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'post_new.html'
+    fields = '__all__'
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title', 'body']
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
 
 
 
